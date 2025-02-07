621207d88430d75d87053901aeb66e4f4e0a95b0
Raul Arribas <raul@exordo.com>
2025-02-07 14:00:05 +0000

    added double click protetction to on submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,28 @@
 import Button from "./Button";
 import styles from "./Form.module.scss";
+import { useState } from 'react';
 
 // eslint-disable-next-line react/prop-types
 export default function Form({onFormSubmit, children}){
+  const [isSaving, setIsSaving] = useState(false);
+
+  const afterSubmit = (ev) => {    
+    onFormSubmit(ev);
+    setIsSaving(false);
+  };
+
   return (
-    <form className={styles['form']} onSubmit={onFormSubmit}>
+    <form className={styles['form']} onSubmit={afterSubmit}>
       <div>
         {children}
       </div>
       <div>
-        <Button text="Submit" type="submit"/>
+        <Button 
+          text="Submit" 
+          type="submit"
+          disabled={isSaving} 
+          onClick={() => setIsSaving(true)}
+        />
       </div>
     </form>
   )
