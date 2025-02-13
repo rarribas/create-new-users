385efdccafeb877890ea3f44bf86ed35b3431d08
Raul Arribas <raul@exordo.com>
2025-02-13 17:28:36 +0000

    style tweaks

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,10 +3,9 @@ import User from './User';
 import Button from './Button';
 /* eslint-disable react/prop-types */
 export default function List({listData, setUsersData}){
+  
   const deleteListItem = (listItem) => {
-    console.log("Remove", listItem, listData);
     const newItems = listData.filter((item) => item.id !== listItem.id)
-
     setUsersData(newItems);
   }
 
@@ -14,13 +13,12 @@ export default function List({listData, setUsersData}){
     return listData.map((listItem)=>{
       return <li key={listItem.id}>
         <User user={listItem}/>
-          <Button
-            text={`Delete ${listItem.name}`}
-            variant='danger'
-            type="button"
-            // disabled={isSaving}
-            onButtonClick={() => deleteListItem(listItem)}
-          />
+        <Button
+          text={`Delete ${listItem.name}`}
+          variant='danger'
+          type="button"
+          onButtonClick={() => deleteListItem(listItem)}
+        />
       </li>
     })
   }
